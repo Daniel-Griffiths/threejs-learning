16bc9cb163a2fb8754726466169ddb27686c43bd
Daniel <daniel.griffiths.jobs@gmail.com>
2021-09-17 17:36:30 +0100

    Finish lesson 7

--- 03-basic-scene/script.ts
+++ 03-basic-scene/script.ts
@@ -1,8 +1,18 @@
 import * as THREE from "three";
 
+/**
+ * Scene
+ */
 const scene = new THREE.Scene();
 
-// Mesh
+/**
+ * Canvas
+ */
+const canvas = document.querySelector("#webgl");
+
+/**
+ * Mesh
+ */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
@@ -14,13 +24,16 @@ const sizes = {
   height: 600,
 };
 
-// Camera
+/**
+ * Camera
+ */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.setZ(3);
 scene.add(camera);
 
-// Render
-const canvas = document.querySelector("#webgl");
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
